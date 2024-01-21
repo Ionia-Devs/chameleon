d0b8aa23f72cdb626278a239699ba545e41cabca
Michael Slocum <36809369+Balance8@users.noreply.github.com>
2024-01-21 15:16:45 -0800

    update allowed origins (#46)

--- apps/chameleon/.wundergraph/wundergraph.config.ts
+++ apps/chameleon/.wundergraph/wundergraph.config.ts
@@ -3,30 +3,30 @@ import {
   configureWunderGraphApplication,
   cors,
   introspect,
-} from '@wundergraph/sdk';
+} from "@wundergraph/sdk"
 
-import generate from './wundergraph.generate';
-import operations from './wundergraph.operations';
-import server from './wundergraph.server';
+import generate from "./wundergraph.generate"
+import operations from "./wundergraph.operations"
+import server from "./wundergraph.server"
 
 const countries = introspect.graphql({
-  apiNamespace: 'countries',
-  url: 'https://countries.trevorblades.com/',
-});
+  apiNamespace: "countries",
+  url: "https://countries.trevorblades.com/",
+})
 
 const weather = introspect.graphql({
-  id: 'weather',
-  apiNamespace: 'weather',
-  url: 'https://weather-api.wundergraph.com/',
+  id: "weather",
+  apiNamespace: "weather",
+  url: "https://weather-api.wundergraph.com/",
   introspection: {
     pollingIntervalSeconds: 5,
   },
-});
+})
 
 const db = introspect.prisma({
-  apiNamespace: 'db',
-  prismaFilePath: '../../../data-access/db/prisma/schema.prisma',
-});
+  apiNamespace: "db",
+  prismaFilePath: "../../../data-access/db/prisma/schema.prisma",
+})
 
 // configureWunderGraph emits the configuration
 configureWunderGraphApplication({
@@ -37,21 +37,22 @@ configureWunderGraphApplication({
   cors: {
     ...cors.allowAll,
     allowedOrigins:
-      process.env.NODE_ENV === 'production'
+      process.env.NODE_ENV === "production"
         ? [
-            // change this before deploying to production to the actual domain where you're deploying your app
-            new EnvironmentVariable('NEXTAUTH_URL'),
-            'https://www.chameleon.com',
-            'https://chameleon-production.up.railway.app/',
+            new EnvironmentVariable("NEXTAUTH_URL"),
+            new EnvironmentVariable("VERCEL_URL"),
+            "https://www.chameleon.com",
+            "https://chameleon-sandy.vercel.app/",
+            "https://chameleon-production.up.railway.app/",
           ]
         : [
-            'http://localhost:3000',
-            new EnvironmentVariable('WG_ALLOWED_ORIGIN'),
+            "http://localhost:3000",
+            new EnvironmentVariable("WG_ALLOWED_ORIGIN"),
           ],
   },
   security: {
     enableGraphQLEndpoint:
-      process.env.NODE_ENV !== 'production' ||
+      process.env.NODE_ENV !== "production" ||
       process.env.GITPOD_WORKSPACE_ID !== undefined,
   },
-});
+})
