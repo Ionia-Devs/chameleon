5e83be5a86e8b5d7d007b0eb4daacd82c342b00b
Benjamin Bruington <103612769+BBruington@users.noreply.github.com>
2024-01-30 17:42:49 -0500

    57 integrate resend (#66)
    * integrate resend
    
    * integrate sending emails with resend
    
    * send to multiple emaild
    
    * .
    
    * Apply linting changes
    
    * .
    
    * Apply linting changes
    
    * alphabetize env.mjs and add resend api key
    
    * Apply linting changes
    
    * import env
    
    * Apply linting changes
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ apps/chameleon/app/api/send/route.ts
@@ -0,0 +1,34 @@
+import { Resend } from 'resend'
+
+import { env } from '@/env.mjs'
+import { EmailTemplate } from '@/components/email-template'
+
+const resend = new Resend(env.RESEND_API_KEY)
+
+interface RequestBody {
+  emails: string[]
+  name: string
+  message: string
+  subject: string
+}
+
+export async function POST(request: Request) {
+  try {
+    const body: RequestBody = await request.json()
+    const { emails, name, message, subject } = body
+    const data = await resend.emails.send({
+      from: `${name ? name : 'name'} <resendEmail@devscape.gg>`,
+      to: emails,
+      subject: subject,
+      react: EmailTemplate({
+        name,
+        message,
+        emails,
+        subject,
+      }) as React.ReactElement,
+    })
+    return Response.json(data)
+  } catch (error) {
+    return Response.json({ error })
+  }
+}
