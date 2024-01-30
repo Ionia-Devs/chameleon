28a893e1d1121b6f21d6cb958347f24856811068
BIBruington <psychological_chemist@hotmail.com>
2024-01-30 16:38:38 -0500

    send to multiple emaild

--- apps/chameleon/app/api/send/route.ts
+++ apps/chameleon/app/api/send/route.ts
@@ -4,7 +4,7 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 interface RequestBody {
-  email: string;
+  emails: string[];
   name: string;
   message: string;
   subject: string;
@@ -13,10 +13,10 @@ interface RequestBody {
 export async function POST(request: Request) {
   try {
     const body: RequestBody = await request.json()
-    const {email, name, message, subject} = body
+    const {emails, name, message, subject} = body
     const data = await resend.emails.send({
       from: `${name ? name : "name"} <resendEmail@devscape.gg>`,
-      to: [email],
+      to: [...emails],
       subject: subject,
       react: EmailTemplate({ name, message }) as React.ReactElement,
     });
