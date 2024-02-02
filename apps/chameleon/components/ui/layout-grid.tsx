b7cf5edf0e9a8b1250aa008f9559d54dd713518c
BIBruington <psychological_chemist@hotmail.com>
2024-02-02 15:48:33 -0500

    i figured since the images have a fixed position to pop up in the atom, the easiest solution would be to have a scrollbar for the images

--- apps/chameleon/components/ui/layout-grid.tsx
+++ apps/chameleon/components/ui/layout-grid.tsx
@@ -1,9 +1,8 @@
-'use client'
-
-import React, { useState } from 'react'
-import Image from 'next/image'
-import { cn } from '@/utils/cn'
-import { motion } from 'framer-motion'
+"use client";
+import React, { useState, } from "react";
+import { motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 type Card = {
   id: number | string
