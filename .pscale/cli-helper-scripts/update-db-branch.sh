564979efca627c0e8174c75e19de0d7b61f481f7
Michael Slocum <mslocum819@gmail.com>
2024-02-03 23:58:52 -0800

    Update schema change script to include database URL

--- .pscale/cli-helper-scripts/update-db-branch.sh
+++ .pscale/cli-helper-scripts/update-db-branch.sh
@@ -12,5 +12,5 @@ DEPLOY_REQUEST_NUMBER="$3"
 . set-db-and-org-and-branch-name.sh
 
 . ps-create-helper-functions.sh
-create-schema-change "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "$DDL_STATEMENTS"
+create-schema-change "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "$DDL_STATEMENTS" "$MY_DB_URL"
 create-diff-for-ci "$DB_NAME" "$ORG_NAME" "$DEPLOY_REQUEST_NUMBER" "$BRANCH_NAME" "update"
