4ddc394b53d49939cc97ee7f4c1b07d97f80f75b
Michael Slocum <mslocum819@gmail.com>
2024-02-04 00:13:48 -0800

    Update Git actions and database branch connection string

--- .pscale/cli-helper-scripts/update-db-branch.sh
+++ .pscale/cli-helper-scripts/update-db-branch.sh
@@ -8,11 +8,12 @@ BRANCH_NAME="$1"
 DDL_STATEMENTS="$2"
 DEPLOY_REQUEST_NUMBER="$3"
 
-
 . set-db-and-org-and-branch-name.sh
 
+UNIQUE_ID=$(uuidgen)
+
 . create-branch-connection-string.sh
-create-branch-connection-string "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "creds-${BRANCH_NAME}" 
+create-branch-connection-string "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "creds-${BRANCH_NAME}-${UNIQUE_ID}" 
 
 . ps-create-helper-functions.sh
 create-schema-change "$DB_NAME" "$BRANCH_NAME" "$ORG_NAME" "$DDL_STATEMENTS" "$MY_DB_URL"
