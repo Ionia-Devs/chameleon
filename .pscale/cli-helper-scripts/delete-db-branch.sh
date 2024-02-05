b2bbcdd3f04a507e1bdb4c830b57a3cd50adfcc0
Michael Slocum <36809369+Balance8@users.noreply.github.com>
2024-02-04 19:15:38 -0800

    test prisma action (#78)
    * test prisma action
    
    ---------
    
    Co-authored-by: Balance8 <Balance8@users.noreply.github.com>

--- .pscale/cli-helper-scripts/delete-db-branch.sh
+++ .pscale/cli-helper-scripts/delete-db-branch.sh
@@ -9,3 +9,11 @@ BRANCH_NAME="$1"
 . set-db-and-org-and-branch-name.sh
 
 pscale branch delete "$DB_NAME" "$BRANCH_NAME" --force --org "$ORG_NAME"
+
+ENV_FILE_PATH=".pscale/env/ps-env-${BRANCH_NAME}.sh"
+if [ -f "$ENV_FILE_PATH" ]; then
+    echo "Deleting environment file: $ENV_FILE_PATH"
+    rm "$ENV_FILE_PATH"
+else
+    echo "Environment file does not exist: $ENV_FILE_PATH"
+fi
