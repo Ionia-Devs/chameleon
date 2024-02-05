964b760880ccebf246862fc2f3dd7b72bed488ed
Michael Slocum <36809369+Balance8@users.noreply.github.com>
2024-02-04 19:23:29 -0800

    update-action (#79)
    * update-action
    
    * Add PS environment for branch 02-04-update-action
    
    ---------
    
    Co-authored-by: Balance8 <Balance8@users.noreply.github.com>

---
+++ .pscale/env/ps-env-02-04-update-action.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export ORG_NAME="chameleon-la"
+echo "::set-output name=ORG_NAME::chameleon-la"
+
+export DB_NAME="chameleon"
+echo "::set-output name=DB_NAME::chameleon"
+
+export BRANCH_NAME="02-04-update-action"
+echo "::set-output name=BRANCH_NAME::02-04-update-action"
+
+export DEPLOY_REQUEST_NUMBER="18"
+echo "::set-output name=DEPLOY_REQUEST_NUMBER::18"
+
+export DEPLOY_REQUEST_URL="https://app.planetscale.com/chameleon-la/chameleon/deploy-requests/18"
+echo "::set-output name=DEPLOY_REQUEST_URL::https://app.planetscale.com/chameleon-la/chameleon/deploy-requests/18"
+
+export BRANCH_URL="https://app.planetscale.com/chameleon-la/chameleon/02-04-update-action"
+echo "::set-output name=BRANCH_URL::https://app.planetscale.com/chameleon-la/chameleon/02-04-update-action"
+
