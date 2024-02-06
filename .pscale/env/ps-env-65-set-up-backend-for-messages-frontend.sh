839f1f83169f0248dc74f3c40053dff8705e2c90
Balance8 <Balance8@users.noreply.github.com>
2024-02-06 02:44:07 +0000

    Add PS environment for branch 65-set-up-backend-for-messages-frontend

---
+++ .pscale/env/ps-env-65-set-up-backend-for-messages-frontend.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export ORG_NAME="chameleon-la"
+echo "::set-output name=ORG_NAME::chameleon-la"
+
+export DB_NAME="chameleon"
+echo "::set-output name=DB_NAME::chameleon"
+
+export BRANCH_NAME="65-set-up-backend-for-messages-frontend"
+echo "::set-output name=BRANCH_NAME::65-set-up-backend-for-messages-frontend"
+
+export DEPLOY_REQUEST_NUMBER="20"
+echo "::set-output name=DEPLOY_REQUEST_NUMBER::20"
+
+export DEPLOY_REQUEST_URL="https://app.planetscale.com/chameleon-la/chameleon/deploy-requests/20"
+echo "::set-output name=DEPLOY_REQUEST_URL::https://app.planetscale.com/chameleon-la/chameleon/deploy-requests/20"
+
+export BRANCH_URL="https://app.planetscale.com/chameleon-la/chameleon/65-set-up-backend-for-messages-frontend"
+echo "::set-output name=BRANCH_URL::https://app.planetscale.com/chameleon-la/chameleon/65-set-up-backend-for-messages-frontend"
+
