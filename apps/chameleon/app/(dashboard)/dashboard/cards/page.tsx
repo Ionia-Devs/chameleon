ccc75a8c8e5c4c8743e0ca4d49cfc15f4efe570c
Michael Slocum <mslocum819@gmail.com>
2024-02-07 18:35:16 -0800

    Remove unused Image import and update grid layout in CardsPage component

--- apps/chameleon/app/(dashboard)/dashboard/cards/page.tsx
+++ apps/chameleon/app/(dashboard)/dashboard/cards/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from 'next'
-import Image from 'next/image'
 
 import { cn } from '@/lib/utils'
 
@@ -36,23 +35,7 @@ function DemoContainer({
 export default function CardsPage() {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/cards-light.png"
-          width={1280}
-          height={1214}
-          alt="Cards"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/cards-dark.png"
-          width={1280}
-          height={1214}
-          alt="Cards"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden items-start justify-center gap-6 rounded-lg p-8 md:grid lg:grid-cols-2 xl:grid-cols-3">
+      <div className="items-start justify-center gap-6 rounded-lg p-8 md:grid lg:grid-cols-2">
         <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
           <DemoContainer>
             <DemoCreateAccount />
