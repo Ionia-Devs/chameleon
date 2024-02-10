7c114368ef356effa4f5ee9618118b290927f460
BIBruington <psychological_chemist@hotmail.com>
2024-02-09 23:30:45 -0500

    reformatted portfolio for smaller screens

--- apps/chameleon/app/(profile)/profile/edit/_components/portfolio-image.tsx
+++ apps/chameleon/app/(profile)/profile/edit/_components/portfolio-image.tsx
@@ -13,13 +13,13 @@ export default function Portfolio({ picture }: PortfolioProps) {
     await handleRemovePhoto({image: {id: picture.id}})
   }
   return (
-    <div className='group relative border-2 border-accent rounded-lg'>
+    <div className='group relative h-80 w-full'>
       <Image
         alt="portfolio picture"
         src={picture.image}
         height="500"
         width="500"
-        className="h-72 w-72 object-cover object-left-top rounded-lg gap-5 !m-0 !p-0"
+        className="object-cover object-top absolute inset-0 h-80 w-full border-2 border-accent"
       />
       <button onClick={removePhoto} className='absolute top-0 right-0 m-1 mr-2 z-10 group-hover:visible invisible'><XIcon className='bg-secondary' /></button>
     </div>
--- apps/chameleon/app/(profile)/profile/edit/page.tsx
+++ apps/chameleon/app/(profile)/profile/edit/page.tsx
@@ -157,7 +157,7 @@ export default async function EditProfile() {
       </section>
       <div className="flex flex-col mt-5 md:mt-0">
         <Label className="flex self-center text-3xl">Portfolio</Label>
-        <div className="h-[40rem] items-start overflow-y-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-5xl mx-auto gap-10 py-40 px-10">
+        <div className="h-screen py-20 mt-3 w-full p-10 overflow-y-auto border-2 border-accent grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  max-w-7xl mx-auto gap-4 ">
           {userProfileData?.Portfolio.length !== undefined && userProfileData?.Portfolio.length > 0 ?  (userProfileData?.Portfolio.map((card, index) => (
             <Portfolio key={index} picture={card} />
           ))) : <div></div>}
