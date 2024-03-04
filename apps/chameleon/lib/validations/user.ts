5a2c0819bc8c2c44869cd90336099631f7bc01d6
BIBruington <psychological_chemist@hotmail.com>
2024-03-03 20:52:53 -0500

    client side validation / shadcn form

--- apps/chameleon/lib/validations/user.ts
+++ apps/chameleon/lib/validations/user.ts
@@ -1,5 +1,10 @@
 import * as z from 'zod'
 
 export const userNameSchema = z.object({
-  name: z.string().min(3).max(32),
+  name: z
+    .string()
+    .min(3, { message: 'Username must be at least 3 characters.' })
+    .max(32, {
+      message: 'Username is too long. It must be under 32 characters.',
+    }),
 })
